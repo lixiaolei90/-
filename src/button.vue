35f963a43e23cd86baac0ba669db0d2aa94c12bd
li.xiaolei <lixiaolei327@gmail.com>
2020-09-02 20:52:33 +0800

    用parcel将button组件改成单文件组件

---
+++ src/button.vue
@@ -0,0 +1,36 @@
+<template>
+  <button class="l-button">按钮</button>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+body {
+  font-size: var(--font-size);
+}
+#app {
+  margin: 20px;
+}
+.l-button {
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 1em;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+  &:hover {
+    border-color: var(--border-color-hover);
+  }
+  &:active {
+    background: var(--button-active-bg);
+  }
+  &:focus {
+    outline: none;
+  }
+}
+
+</style>
