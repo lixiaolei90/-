692faf311c4851340927662c0ebbd265f56ceeee
li.xiaolei <lixiaolei327@gmail.com>
2020-09-03 19:49:53 +0800

    添加icon和iconposition

--- src/button.vue
+++ src/button.vue
@@ -1,10 +1,19 @@
 <template>
-  <button class="l-button">按钮</button>
+  <div>
+    <button class="l-button" :class="{[`icon-${iconPosition}`]: true}">
+      <svg v-if="icon" class="icon">
+        <use :xlink:href="`#i-${icon}`"></use>
+      </svg>
+      <div class="content">
+        <slot></slot>
+      </div>
+    </button>
+  </div>
 </template>
 
 <script>
 export default {
-
+  props: [`icon`, 'iconPosition']
 }
 </script>
 
@@ -12,9 +21,12 @@ export default {
 body {
   font-size: var(--font-size);
 }
+
 #app {
   margin: 20px;
+  display: flex;
 }
+
 .l-button {
   font-size: var(--font-size);
   height: var(--button-height);
@@ -22,15 +34,44 @@ body {
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
+  margin: 0 0.5em;
   &:hover {
     border-color: var(--border-color-hover);
   }
+
   &:active {
     background: var(--button-active-bg);
   }
+
   &:focus {
     outline: none;
   }
-}
 
+
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  //vertical-align: middle;
+  >.content{
+    order: 2;}
+  >.icon{
+    margin-right:0.3em;
+    order:1;
+  }
+  &.icon-right{
+    >.content{
+      order:1;
+    }
+    >.icon{
+      order:2;
+      margin-left: 0.3em;
+      margin-right: 0;
+    }
+  }
+
+}
+  .icon {
+    width: 1em;
+    height: 1em;
+  }
 </style>
